e28a9a77a04283332510cf7ca87e5e67aa861bf5
Maximilian Grabau <max@grabau.dev>
2024-10-30 09:31:57 +0100

    Organize imports

---
+++ src/components/layout/index.ts
@@ -0,0 +1,3 @@
+export {default as Navigation } from './Navigation.astro';
+export {default as NavLink } from './NavLink.astro';
+export {default as Footer } from './Footer.astro';
---
+++ src/components/sections/index.ts
@@ -0,0 +1,4 @@
+export {default as Section } from './Section.astro';
+export {default as BlogSection } from './BlogSection.astro';
+export {default as AboutSection } from './AboutSection.astro';
+export {default as ContactSection } from './ContactSection.astro';
