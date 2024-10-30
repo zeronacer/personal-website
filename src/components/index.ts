2266a919bc8ca6c2faf055cae7dc8f3827aff37d
Maximilian Grabau <max@grabau.dev>
2024-10-30 09:31:57 +0100

    Organize imports

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as MouseTrailer } from "./MouseTrailer.astro";
+export { default as Button } from "./Button.astro";
+export { default as BlogPost } from "./BlogPost.astro";
+
+export { default as Pill } from "./pills/Pill.astro";
+export { default as Pills } from "./pills/Pills.astro";
